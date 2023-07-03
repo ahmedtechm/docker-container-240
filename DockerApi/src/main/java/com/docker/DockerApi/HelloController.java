c77423a9c01e2ef7903a8ac0127d6c57856aa149
ahmedtechm <ahmedtechm2050@gmail.com>
2023-07-03 17:03:57 +0400

    Added docker container using spring boot project

---
+++ DockerApi/src/main/java/com/docker/DockerApi/HelloController.java
@@ -0,0 +1,5 @@
+package com.docker.DockerApi;
+
+public class HelloController {
+
+}
---
+++ DockerApi/src/test/java/com/docker/DockerApi/DockerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.DockerApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
